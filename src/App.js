939c94cff7883afdfcc1e89d8d1693defa932810
pauleezz <106306033@nccu.edu.tw>
2020-05-06 16:34:22 +0800

    add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>智能投資模擬平台</h1>
+      <form>
+        <div>
+          <input type="number" />
+        </div>
+        <div>
+          <select name="Strategy">
+            <option value="Taipei">台北</option>　
+            <option value="Taoyuan">桃園</option>　
+            <option value="Hsinchu">新竹</option>　
+            <option value="Miaoli">苗栗</option>
+          </select>
+        </div>
+        <div>
+          <button>模擬</button>
+        </div>
+      </form>
+      <div>
+        <h1>結果</h1>
+      </div>
     </div>
   );
 }
