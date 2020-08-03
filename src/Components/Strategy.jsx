6de9f8ee9cf3c9dc71cef0f2c69e67208f8ad36a
pauleezz <106306033@nccu.edu.tw>
2020-08-03 20:05:14 +0800

    0803 add d3.csv 2

--- src/Components/Strategy.jsx
+++ src/Components/Strategy.jsx
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 
 class Strategy extends Component {
   getData = () => {
-    d3.csv(, function (data) {
+    d3.csv('https://raw.githubusercontent.com/soslab106/blockchain/master/src/csv/AssociationStock.csv', function (data) {
       console.log(data);
     });
   };
